3fcf7afe5bb928c558046cd869f704c1cc24e6fc
unknown <fgophi@gmail.com>
2020-07-07 15:56:16 +0900

    FGO game data API 対応

--- csv2report.py
+++ csv2report.py
@@ -4,88 +4,162 @@ import csv
 import sys
 import argparse
 import fgogachacnt
+from pathlib import Path
 
-num_servant_0star = 0
-num_servant_1star = 0
-num_servant_2star = 0
-num_servant_3star = 0
-num_servant_4star = 0
+Servant_file = Path(__file__).resolve().parent / Path("hash_srv.csv")
+CE_file = Path(__file__).resolve().parent / Path("hash_ce.csv")
 
-num_exp_1star = 0
-num_exp_2star = 0
-num_exp_3star = 0
-num_exp_4star = 0
-num_exp_5star = 0
+servant_rarity = {}
+ce_rarity = {}
 
-num_status_1star = 0
-num_status_2star = 0
-num_status_3star = 0
+def make_rarity():
+    """
+    CSVからサーヴァントと概念礼装のレアリティ情報を作成
+    """
+    for i in range(6):
+        servant_rarity[i] = []
+        
+    with open(Servant_file, encoding='UTF-8') as f:
+        reader = csv.reader(f)
+        srvs = [row for row in reader]
+        for srv in srvs:
+            servant_rarity[int(srv[1])] = servant_rarity[int(srv[1])] + [srv[0]]
 
-num_ce_1star = 0
-num_ce_2star = 0
-num_ce_3star = 0
+    for i in range(5):
+        ce_rarity[i+1] = []
 
-num_ccode_1star = 0
-num_ccode_2star = 0
+    with open(CE_file, encoding='UTF-8') as f:
+        reader = csv.reader(f)
+        ces = [row for row in reader]
+        for ce in ces:
+            ce_rarity[int(ce[1])] = ce_rarity[int(ce[1])] + [ce[0]]
 
-num_summon = 0
+def make_data():
+    """
+    報告内容を作成
+    """
+    mode = "fp"
+    num_servant_0star = 0
+    num_servant_1star = 0
+    num_servant_2star = 0
+    num_servant_3star = 0
+    num_servant_4star = 0
+    num_servant_5star = 0
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('infile', nargs='?', type=argparse.FileType(),
-                        default=sys.stdin)
-    args = parser.parse_args()
+    num_exp_1star = 0
+    num_exp_2star = 0
+    num_exp_3star = 0
+    num_exp_4star = 0
+    num_exp_5star = 0
 
+    num_status_1star = 0
+    num_status_2star = 0
+    num_status_3star = 0
 
+    num_ce_1star = 0
+    num_ce_2star = 0
+    num_ce_3star = 0
+    num_ce_4star = 0
+    num_ce_5star = 0
+
+    num_kalesco = 0
+
+    num_ccode_1star = 0
+    num_ccode_2star = 0
+
+    num_summon = 0
+    result = ""
+    
     with args.infile as f:
         reader = csv.DictReader(f)
         l = [row for row in reader]
 
+    if l[2]['召喚数'] == "11":
+        mode = "stone"
+
     for i, item in enumerate(l[0].keys()):
         if item == "召喚数":
             num_summon = l[0]['召喚数']
-        if item in fgogachacnt.servant_0star:
+
+        if mode == "stone":
+            if item in servant_rarity[3]:
+                num_servant_3star = num_servant_3star + int(l[0][item])
+            elif item in servant_rarity[4]:
+                num_servant_4star = num_servant_4star + int(l[0][item])
+            elif item in servant_rarity[5]:
+                num_servant_5star = num_servant_5star + int(l[0][item])
+            elif item in ce_rarity[3]:
+                num_ce_3star = num_ce_3star + int(l[0][item])
+            elif item in ce_rarity[4]:
+                num_ce_4star = num_ce_4star + int(l[0][item])
+            elif item in ce_rarity[5]:
+                num_ce_5star = num_ce_5star + int(l[0][item])
+
+            if item == "カレイドスコープ":
+                num_kalesco = num_kalesco + int(l[0][item])
+            continue
+
+        if item in servant_rarity[0]:
             num_servant_0star = num_servant_0star + int(l[0][item])
-        if item in fgogachacnt.servant_1star:
+        elif item in servant_rarity[1]:
             num_servant_1star = num_servant_1star + int(l[0][item])
-        if item in fgogachacnt.servant_2star:
+        elif item in servant_rarity[2]:
             num_servant_2star = num_servant_2star + int(l[0][item])
-        if item in fgogachacnt.servant_3star:
+        elif item in servant_rarity[3]:
             num_servant_3star = num_servant_3star + int(l[0][item])
-        if item in fgogachacnt.servant_4star:
+        elif item in servant_rarity[4]:
             num_servant_4star = num_servant_4star + int(l[0][item])
 
-        if item in fgogachacnt.exp_1star:
+        elif item in fgogachacnt.exp_1star:
             num_exp_1star = num_exp_1star + int(l[0][item])
-        if item in fgogachacnt.exp_2star:
+        elif item in fgogachacnt.exp_2star:
             num_exp_2star = num_exp_2star + int(l[0][item])
-        if item in fgogachacnt.exp_3star:
+        elif item in fgogachacnt.exp_3star:
             num_exp_3star = num_exp_3star + int(l[0][item])
-        if item in fgogachacnt.exp_4star:
+        elif item in fgogachacnt.exp_4star:
             num_exp_4star = num_exp_4star + int(l[0][item])
-        if item in fgogachacnt.exp_5star:
+        elif item in fgogachacnt.exp_5star:
             num_exp_5star = num_exp_5star + int(l[0][item])
 
-        if item in fgogachacnt.status_1star:
+        elif item in fgogachacnt.status_1star:
             num_status_1star = num_status_1star + int(l[0][item])
-        if item in fgogachacnt.status_2star:
+        elif item in fgogachacnt.status_2star:
             num_status_2star = num_status_2star + int(l[0][item])
-        if item in fgogachacnt.status_3star:
+        elif item in fgogachacnt.status_3star:
             num_status_3star = num_status_3star + int(l[0][item])
 
-        if item in fgogachacnt.ce_1star:
+        elif item in ce_rarity[1]:
             num_ce_1star = num_ce_1star + int(l[0][item])
-        if item in fgogachacnt.ce_2star:
+        elif item in ce_rarity[2]:
             num_ce_2star = num_ce_2star + int(l[0][item])
-        if item in fgogachacnt.ce_3star:
+        elif item in ce_rarity[3]:
             num_ce_3star = num_ce_3star + int(l[0][item])
 
-        if item in fgogachacnt.ccode_1star:
+        elif item in fgogachacnt.ccode_1star:
             num_ccode_1star = num_ccode_1star + int(l[0][item])
-        if item in fgogachacnt.ccode_2star:
+        elif item in fgogachacnt.ccode_2star:
             num_ccode_2star = num_ccode_2star + int(l[0][item])
 
+    if mode == "stone":
+        sum_std =  num_servant_3star +num_servant_4star  +num_servant_5star \
+                + num_ce_3star + num_ce_4star + num_ce_5star
 
+        result = """【聖晶石召喚】{}回
+★3鯖{}-★4鯖{}-★5鯖1{}
+★3礼装{}-★4礼装{}-★5礼装{}(うちカレスコ{})
+""".format(num_summon,
+           num_servant_3star, num_servant_4star, num_servant_5star,
+           num_ce_3star,num_ce_4star,num_ce_5star,
+           num_kalesco)
+
+        summon_diff = int(num_summon) - sum_std
+        if summon_diff > 0:
+            result = result + "その他" + str(summon_diff) + "\n"
+
+        result = result + "#FGO_聖晶石召喚報告"
+
+        return result
+    
     sum_std = num_servant_0star + num_servant_1star + num_servant_2star \
           + num_servant_3star +num_servant_4star \
             + num_exp_1star + num_exp_2star + num_exp_3star \
@@ -93,53 +167,34 @@ if __name__ == '__main__':
             + num_status_1star + num_status_2star + num_status_3star \
             + num_ce_1star + num_ce_2star + num_ce_3star \
             +num_ccode_1star + num_ccode_2star
-    print("【フレンドポイント召喚】", end="")
-    print(num_summon, end="回\n")
 
-    print("★0鯖",end="")
-    print(num_servant_0star, end="-")
-    print("★1鯖",end="")
-    print(num_servant_1star, end="-")
-    print("★2鯖",end="")
-    print(num_servant_2star, end="-")
-    print("★3鯖",end="")
-    print(num_servant_3star, end="-")
-    print("★4鯖",end="")
-    print(num_servant_4star)
-
-    print("★1種火",end="")
-    print(num_exp_1star, end="-")
-    print("★2種火",end="")
-    print(num_exp_2star, end="-")
-    print("★3種火",end="")
-    print(num_exp_3star, end="-")
-    print("★4種火",end="")
-    print(num_exp_4star, end="-")
-    print("★5種火",end="")
-    print(num_exp_5star)
-
-    print("★1フォウ",end="")
-    print(num_status_1star, end="-")
-    print("★2フォウ",end="")
-    print(num_status_2star, end="-")
-    print("★3フォウ",end="")
-    print(num_status_3star)
-
-    print("★1礼装",end="")
-    print(num_ce_1star, end="-")
-    print("★2礼装",end="")
-    print(num_ce_2star, end="-")
-    print("★3礼装",end="")
-    print(num_ce_3star)
-
-    print("★1コード",end="")
-    print(num_ccode_1star, end="-")
-    print("★2コード",end="")
-    print(num_ccode_2star)
+    result = """【フレンドポイント召喚】{}回
+★0鯖{}-★1鯖{}-★2鯖{}-★3鯖{}-★4鯖{}
+★1種火{}-★2種火{}-★3種火{}-★4種火{}-★5種火{}
+★1フォウ{}-★2フォウ{}-★3フォウ{}
+★1礼装{}-★2礼装{}-★3礼装{}
+★1コード{}-★2コード{}
+""".format(num_summon,
+           num_servant_0star, num_servant_1star, num_servant_2star, num_servant_3star, num_servant_4star,
+           num_exp_1star, num_exp_2star, num_exp_3star, num_exp_4star, num_exp_5star,
+           num_status_1star, num_status_2star, num_status_3star,
+           num_ce_1star, num_ce_2star, num_ce_3star,
+           num_ccode_1star, num_ccode_2star)
 
     summon_diff = int(num_summon) - sum_std
     if summon_diff > 0:
-        print("その他",end="")
-        print(summon_diff)
-        
-    print("#FGO_FP召喚報告")
+        result = result + "その他" + str(summon_diff) + "\n"
+
+    result = result + "#FGO_聖晶石召喚報告"
+
+    return result
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('infile', nargs='?', type=argparse.FileType(),
+                        default=sys.stdin)
+    args = parser.parse_args()
+    make_rarity()
+    result = make_data()
+    print(result)
+

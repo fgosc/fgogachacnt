000aa6f67177517908e456e52a6b6f5578e3b0e2
Takanori Kojima <takanori.kojima@gmail.com>
2021-08-19 00:05:10 +0900

    オプション -m, -n の廃止を GUI に反映。

--- gui.py
+++ gui.py
@@ -7,10 +7,6 @@ import PySimpleGUIWx as sg
 
 KEY_TARGET_FOLDER = "TargetFolder"
 KEY_EXEC_BUTTON = "ExecButton"
-KEY_SUMMON_MODE = "SummonMode"
-KEY_SUMMON_MODE_FP = "SummonModeRadioFPNormal"
-KEY_SUMMON_MODE_FP_YAMATAI = "SummonModeRadioFPYamatai"
-KEY_SUMMON_MODE_SQ = "SummonModeRadioSQ"
 KEY_ENABLE_DEBUGMODE = "EnableDebugging"
 KEY_EXEC_RESULT_OUTPUT = "ResultOutput"
 
@@ -23,12 +19,6 @@ def make_window(theme):
             sg.Text("フォルダ"),
             sg.InputText(key=KEY_TARGET_FOLDER), sg.FolderBrowse(),
             sg.Checkbox("デバッグ出力を有効にする", default=False, pad=(20, 0), key=KEY_ENABLE_DEBUGMODE),
-        ],
-        [
-            sg.Text("召喚モード"),
-            sg.Radio("フレンドポイント召喚", KEY_SUMMON_MODE, size=(15, 1), default=True, key=KEY_SUMMON_MODE_FP),
-            sg.Radio("邪馬台国限定FP召喚", KEY_SUMMON_MODE, size=(16, 1), key=KEY_SUMMON_MODE_FP_YAMATAI),
-            sg.Radio("聖晶石召喚", KEY_SUMMON_MODE, size=(10, 1), key=KEY_SUMMON_MODE_SQ),
         ],
         [sg.Submit("実行", key=KEY_EXEC_BUTTON)],
         [sg.Text("\n実行結果（Twitter投稿用）")],
@@ -52,14 +42,10 @@ def run_command(cmd, input=None):
     return proc.stdout
 
 
-def run_fgogachacnt(mode, num, folder, enable_debug):
+def run_fgogachacnt(folder, enable_debug):
     cmd = [
         "gui\\python\\python",
         "fgogachacnt.py",
-        "-m",
-        mode,
-        "-n",
-        num,
         "-f",
         folder,
     ]
@@ -100,31 +86,12 @@ def main():
                 sg.popup("フォルダ内に画像ファイルが見つかりません。")
                 continue
 
-            if values[KEY_SUMMON_MODE_FP]:
-                mode = "fp"
-                num = "10"
-                report_option = ""
-
-            elif values[KEY_SUMMON_MODE_FP_YAMATAI]:
-                mode = "fp"
-                num = "10"
-                report_option = "--yamataikoku"
-
-            elif values[KEY_SUMMON_MODE_SQ]:
-                mode = "stone"
-                num = "11"
-                report_option = ""
-
-            else:
-                sg.popup("予期しないエラーが発生しました。")
-                continue
-
             if values[KEY_ENABLE_DEBUGMODE]:
                 enable_debug = True
             else:
                 enable_debug = False
 
-            out = run_fgogachacnt(mode, num, targetFolder, enable_debug)
+            out = run_fgogachacnt(targetFolder, enable_debug)
             result = run_csv2report(out, report_option)
 
             window[KEY_EXEC_RESULT_OUTPUT].update(result.decode("cp932"))
